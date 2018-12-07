a1476f02d0cae019662d2375afb5d5e8b3a3f31f
jammiM <jammim19@gmail.com>
2018-12-07 23:52:23 +0000

    Added browser-sync, so now all changes to html and css updates asap

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
+const sync = require('browser-sync').create();
 
 gulp.task('message', function(){
   return console.log("blah");
@@ -12,22 +13,44 @@ gulp.task('copyHtml', () =>
     .pipe(gulp.dest('dist'))
 );
 
+//Compresses all images within the src/img folder
 gulp.task('img', () =>
   gulp.src('src/img/*')
       .pipe(imagemin())
       .pipe(gulp.dest('dist/img'))
 );
 
+//Compresses all js within the src/js folder
 gulp.task('js', () =>
   gulp.src('src/js/*.js')
       .pipe(uglify())
       .pipe(gulp.dest('dist/js'))
 );
 
+//Compiles all the sass files
 gulp.task('sass', () =>
   gulp.src('src/scss/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('dist/css'))
+    //  .pipe(sync.stream())
 );
 
-gulp.task('default', ['img', 'js', 'sass', 'message', 'copyHtml']);
+
+
+// gulp.task('serve', ['sass'], function () {
+gulp.task('serve', function () {
+
+  // Starts the serve
+  sync.init({
+    //Where the server is served from
+    server: './dist'
+  })
+
+  // Watches all changes in scss and html files
+  gulp.watch('src/scss/*.scss', ['sass']).on('change', sync.reload);
+  gulp.watch('src/*.html', ['copyHtml']).on('change', sync.reload);
+
+})
+
+
+gulp.task('default', ['img', 'js', 'sass', 'message', 'copyHtml', 'serve']);
